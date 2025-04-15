d5441b8bac7c592ed1c1e3fbb7faef5b075e9528
Идрисов Ярослав <pepeloek@gmail.com>
2025-04-15 11:03:10 +0500

    я опять построил гит репозиторий

--- LAB4/FormListTypes.cs
+++ LAB4/FormListTypes.cs
@@ -1,6 +1,8 @@
-﻿using LAB4.Models;
+﻿using System.ComponentModel;
+using LAB4.Models;
 using Microsoft.EntityFrameworkCore;
 using AppContext = LAB4.Models.AppContext;
+using Type = LAB4.Models.Type;
 
 namespace LAB4
 {
@@ -26,10 +28,25 @@ namespace LAB4
             dataGridViewTypes.Columns["TypeName"].HeaderText = "Тип аниме";
         }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.db?.Dispose();
+            this.db = null; 
+
+        }
         private void BtnAddType_Click(object sender, EventArgs e)
         {
-            FormAddType formAddType = new FormAddType();
-            formAddType.ShowDialog();
+            FormAddType formAddType = new();
+            DialogResult result = formAddType.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
+            {
+                return;
+            Type type = new Type();
+                type.TypeName
+            }
+            
         }
     }
 }
